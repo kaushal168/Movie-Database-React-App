250fcb17a707017483ef1e7636bc12c42aa3adbc
Kaushal Baid <kaushalbaid16@gmail.com>
2021-08-03 20:57:03 +0530

    Throttling API requests implemented

--- src/context.js
+++ src/context.js
@@ -1,40 +1,53 @@
+import axios from "axios";
 import React, { useState, useContext, useEffect } from "react"
 import "./index.css"
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState({ show: false, msg: '' })
   const [movies, setMovies] = useState([])
   const [query, setQuery] = useState('')
   
-  const fetchMovies = async (url) => {
-    setIsLoading(true)
-    try {
-      const response = await fetch(url)
-      const data = await response.json()
-      if (data.Response === 'True') {
-        setMovies(data.Search)
-        setError(false)
-      }
-      else {
-        if(query !== ''){
-          setError({ show: true, msg: data.Error })
+  useEffect(() => {
+    const fetchMovies = async () => {
+      setIsLoading(true)
+      try{
+        const response = await axios.get(`${API_ENDPOINT}&s=${query}`)
+        if (response.data.Response === 'True') {
+          setMovies(response.data.Search)
+          setError(false)
         }
+        else {
+          if(query !== ''){
+            setError({ show: true, msg: response.data.Error })
+          }
+        }
+        setIsLoading(false)
+      } catch (error) {
+        console.log(error)
       }
-      setIsLoading(false)
-    } catch (error) {
-      console.log(error)
     }
     if(query === ''){
       setMovies([])
-      setError({ show: false })
+      setError(false)
     }
-  }
-
-  useEffect(() => {
-    fetchMovies(`${API_ENDPOINT}&s=${query}`)
+    //run without throttling search requests if initial page load
+    if(query && !movies.length){
+      fetchMovies()
+    }
+    else {//else throttle search requests
+      let timeoutId = setTimeout(() => {
+        if(query){
+          fetchMovies()
+        }
+      }, 500)
+      
+      return () => {
+        clearTimeout(timeoutId)
+      }
+    }
     // eslint-disable-next-line
   }, [query])
 
